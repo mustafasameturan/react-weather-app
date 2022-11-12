eb05066a55c558ac6de3fd67efb61d213ecfb31c
Mustafa Samet Turan <mustafasameturan@hotmail.com>
2022-11-12 17:07:31 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Search from "./components/Search";
+import CurrentWeather from "./components/CurrentWeather";
+import Forecast from "./components/Forecast";
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [currentForecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${process.env.REACT_APP_WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${process.env.REACT_APP_WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+      <div className="container">
+
+        <Search onSearchChange={handleOnSearchChange} />
+
+        {currentWeather && <CurrentWeather data={currentWeather} />}
+
+        {currentForecast && <Forecast data={currentForecast} />}
+      </div>
+  );
+}
+
+export default App;
---
+++ src/services/GeoApiService.js
@@ -0,0 +1,7 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '94ae1ab7eemsh6443a84a210a552p16aa86jsn69be65008b78',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
